5c6b2aab7d68e0286e39b99d553419face702fa4
Yelena Belikova <yelenabel@gmail.com>
2015-08-18 12:26:22 -0700

    create new home page with Spark

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,9 +3,10 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import static java.lang.System.out;
 import java.lang.*;
-
+import java.util.Timer;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.TimerTask;
 
 import spark.ModelAndView;
 
@@ -14,6 +15,23 @@ import static spark.Spark.*;
 
 public class App{
   public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
 
-  }
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    Tamagotchi newTama = new Tamagotchi("Alice");
+    Timer timer = new Timer();
+    timer.scheduleAtFixedRate(new TimerTask() {
+       public void run() {
+         newTama.decrementLevels();
+         System.out.println(newTama.getFoodLevel());
+       }
+     },1000,10000);
+
+ }
 }
