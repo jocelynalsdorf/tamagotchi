298e492d7df0a989a2e57415b78dea9f411b79f9
Yelena Belikova <yelenabel@gmail.com>
2015-08-18 12:27:09 -0700

    pass dest for decrementing values

--- src/test/java/TamagotchiTest.java
+++ src/test/java/TamagotchiTest.java
@@ -8,4 +8,23 @@ public class TamagotchiTest {
     Tamagotchi tama = new Tamagotchi("fifi");
     assertEquals(true, tama instanceof Tamagotchi);
   }
+
+  @Test
+  public void tamagotchi_ReturnsFoodLevel() {
+    Tamagotchi tama = new Tamagotchi("bob");
+    assertEquals(100, tama.getFoodLevel());
+  }
+  @Test
+  public void setSleepLevel_returnsCorrectSleepLevel() {
+    Tamagotchi tama = new Tamagotchi("oak");
+    tama.setSleepLevel(40);
+    assertEquals(40, tama.getSleepLevel());
+  }
+  @Test
+  public void decrementLevels_decrementsCorrectly() {
+    Tamagotchi tama = new Tamagotchi("Shelly");
+    tama.decrementLevels();
+    assertEquals(90, tama.getFoodLevel());
+  }
+
 }
