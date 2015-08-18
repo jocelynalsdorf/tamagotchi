335bcf387bfc57c778ae9df64101c866cf51b5e8
Yelena Belikova <yelenabel@gmail.com>
2015-08-18 11:12:42 -0700

    create private variable for different levels like hunger, activity and sleep

---
+++ src/main/java/Tamagotchi.java
@@ -0,0 +1,12 @@
+public class Tamagotchi {
+
+  private int mFoodLevel = 100;
+  private int mSleepLevel = 100;
+  private int mActivityLevel = 100;
+
+  private String mName;
+
+  public Tamagotchi(String name) {
+    mName = name;
+  }
+}
